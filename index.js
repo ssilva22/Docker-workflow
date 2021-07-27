bb38030db3ca7e0ad536fbe7e882884b3a4b4d97
Sebastian <sebastian@SEBASTIAN-01.local>
2021-07-27 18:40:54 -0400

    scheleton built

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+
+const app = express();
+
+app.get("/" ,(req,res) => {
+    res.send("Hello There!!!!");
+})
+
+const port = process.env.PORT || 3000;
+
+
+
+app.listen(port, (()=>{
+console.log(`listening on port ${port}`)
+}));
