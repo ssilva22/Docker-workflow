8f2aaacc9c8d2f27a410d305c2d17619d941637f
Sebastian <sebastian@SEBASTIAN-01.local>
2021-07-28 16:54:18 -0400

    workflow setup

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
 FROM node:14.17.3
 WORKDIR /app
 COPY package.json .
-RUN npm install
+
+ARG NODE_ENV
+RUN if [ "$NODE_ENV" = "development" ]; \
+        then npm install; \
+        else npm install --only=production; \
+        fi
+
 COPY . ./
-EXPOSE 3000
+ENV PORT 3000
+EXPOSE $PORT
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const mongoose= require("mongoose");
+const {MONGO_USER,MONGO_PASSWORD,MONGO_IP,MONGO_PORT} = require("./config/config");
 
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+    mongoose
+    .connect(mongoURL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        useFindAndModify:false})
+    .then(()=> console.log("successfully connected to the DB"))
+    .catch((err)=>{
+        console.log(err)
+        setTimeout(connectWithRetry,5000)
+    })
+}
+
+connectWithRetry();
+
+
+
 app.get("/" ,(req,res) => {
-    res.send("Hello There!!!!");
+    res.send("Hello There");
 })
 
 const port = process.env.PORT || 3000;
